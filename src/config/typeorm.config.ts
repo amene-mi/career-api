08784ac051dede16f9a8caaf7211949d3f4fc218
amene-mi <a_mohammadi1991@yahoo.com>
2022-02-19 15:16:30 +0330

    create config file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,3 +1,12 @@
 //import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-//export const typeOrmConfig: TypeOrmModuleOptions ={}
+/*export const typeOrmConfig: TypeOrmModuleOptions ={
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'sealand_db',
+    entities: [],
+    synchronize: true,
+}*/
